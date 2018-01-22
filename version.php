c30419c8e18d05eef1144874b1766612c7dbd21e
jmalmsten-panopto <jmalmsten@panopto.com>
2018-01-22 14:59:15 -0800

    2018011800 release code.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2017120500;
+$plugin->version   = 2018011800;
 
 // Requires this Moodle version - 2.7.
 $plugin->requires  = 2014051200;
